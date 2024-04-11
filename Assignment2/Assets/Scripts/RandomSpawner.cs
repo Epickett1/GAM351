364d44eeb9e5cd7693664e4a0ee3720251bb32f6
FrancoCarlos308 <164448657+carlosfranco03@users.noreply.github.com>
2024-04-10 18:09:01 -0700

    it works

--- Assignment2/Assets/Scripts/RandomSpawner.cs
+++ Assignment2/Assets/Scripts/RandomSpawner.cs
@@ -16,21 +16,21 @@ public class RandomSpawner : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+                Vector3 randomSpawnPosition = new Vector3(Random.Range(-80, 80), 65, Random.Range(-80, 80));
                 Instantiate(ship_1Prefab, randomSpawnPosition, Quaternion.identity);
                 totalSpawned++;
             }
 
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+                Vector3 randomSpawnPosition = new Vector3(Random.Range(-80, 80), 65, Random.Range(-80, 80));
                 Instantiate(ship_2Prefab, randomSpawnPosition, Quaternion.identity);
                 totalSpawned++;
             }
 
             if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+                Vector3 randomSpawnPosition = new Vector3(Random.Range(-80, 80), 65, Random.Range(-80, 80));
                 Instantiate(ship_3Prefab, randomSpawnPosition, Quaternion.identity);
                 totalSpawned++;
             }
