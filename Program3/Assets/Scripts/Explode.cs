02e1fe3a13a76fa714909543f054b958d2874d46
Epickett1 <epickett1@arizona.edu>
2024-04-23 11:52:37 -0700

    more changes
    a few more small changes

--- Program3/Assets/Scripts/Explode.cs
+++ Program3/Assets/Scripts/Explode.cs
@@ -6,16 +6,22 @@ public class Explode : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject debris;
+    Rigidbody rb;
+    MeshRenderer mr;
+    MeshCollider mc;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
+        mc = GetComponent<MeshCollider>();
+    }
 
     public void Explosion()
     {
-       Instantiate(explosion, transform.position, Quaternion.identity);
-       StartCoroutine(AfterExplosion());
-    }
-
-    IEnumerator AfterExplosion()
-    {
-        yield return new WaitForSeconds(2);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        rb.useGravity = false;
+        mr.enabled = false;
+        mc.enabled = false;
         Instantiate(debris, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
