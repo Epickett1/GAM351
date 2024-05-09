8880734d5789236a9114cb9cb4b988d414076fe7
Epickett1 <epickett1@arizona.edu>
2024-05-09 02:51:38 -0700

    Revert "Almost done"
    This reverts commit 5d17ad5934cd909b1d315f26dca602d7a7daf0c5.

--- GAM351FinalProj/Assets/Scripts/Damage.cs
+++ GAM351FinalProj/Assets/Scripts/Damage.cs
@@ -6,34 +6,18 @@ public class Damage : MonoBehaviour
 {
     public float damage;
 
-    public List<string> ignoreTags = new List<string>();
-
     public bool destroyOnContact;
 
-    bool ignore = false;
     private void OnCollisionEnter(Collision collision)
     {
         Damageable dm = collision.gameObject.GetComponent<Damageable>();
         if (dm != null )
         {
-            foreach (string tag in ignoreTags){
-                if (dm.CompareTag(tag))
-                {
-                    ignore = true;
-                }
-            }
-            if (!ignore)
-            {
-                dm.Damage(damage);
-            }
+            dm.Damage(damage);
         }
-        if (!ignore)
+        if ( destroyOnContact )
         {
-            if (destroyOnContact)
-            {
-                Destroy(gameObject);
-            }
+            Destroy(gameObject);
         }
-        ignore = false;
     }
 }
