5d17ad5934cd909b1d315f26dca602d7a7daf0c5
Epickett1 <epickett1@arizona.edu>
2024-05-09 02:45:22 -0700

    Almost done
    almost done

--- GAM351FinalProj/Assets/Scripts/Damage.cs
+++ GAM351FinalProj/Assets/Scripts/Damage.cs
@@ -6,18 +6,34 @@ public class Damage : MonoBehaviour
 {
     public float damage;
 
+    public List<string> ignoreTags = new List<string>();
+
     public bool destroyOnContact;
 
+    bool ignore = false;
     private void OnCollisionEnter(Collision collision)
     {
         Damageable dm = collision.gameObject.GetComponent<Damageable>();
         if (dm != null )
         {
-            dm.Damage(damage);
+            foreach (string tag in ignoreTags){
+                if (dm.CompareTag(tag))
+                {
+                    ignore = true;
+                }
+            }
+            if (!ignore)
+            {
+                dm.Damage(damage);
+            }
         }
-        if ( destroyOnContact )
+        if (!ignore)
         {
-            Destroy(gameObject);
+            if (destroyOnContact)
+            {
+                Destroy(gameObject);
+            }
         }
+        ignore = false;
     }
 }
